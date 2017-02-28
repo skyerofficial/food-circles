defc0c8e9ed6d273f855079fff97fb364e5a4cc1
jaikathuria <jaikathuria@live.com>
2017-02-28 14:30:35 -0600

    Added Base Template

---
+++ js/validate.js
@@ -0,0 +1,27 @@
+var isEmpty = function (v) {
+    if(( v == null ) || ( v == "")){
+        return true;
+    }
+    return false;
+};
+
+$(document).ready( function(){
+    $('#theForm').submit(function(e){
+        e.preventDefault();
+        var i = $("#q").val();
+        if(isEmpty(i)){
+            var err = $(".error");
+            err.html("Field Can't be Empty!")
+            err.removeClass("hide");
+            var q = $(".question");
+            $('body').css('background-color','#EF5350');
+            q.addClass("animated shake");
+            window.setTimeout(function(){
+                $('body').css('background-color','#3498db');
+                q.removeClass("animated shake");
+            },1200);
+            return false;
+        }
+        
+    });
+});
